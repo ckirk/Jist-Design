e4b2b19a3480c6e59b89d0ab1ad9317abab688fb
Chris <ckirkinis@gmail.com>
2018-06-25 21:45:21 -0400

    more process updates

--- app/components/Process.js
+++ app/components/Process.js
@@ -10,9 +10,9 @@ const Process = (props) => {
       <h2>Idea</h2>
       <div className='details'>
         <ul>
-          <li>Come to us with your ideas! We specialize in converting ideas into functional, beautiful products.</li>
+          <li>We specialize in converting ideas into functional, beautiful products.</li>
           <li>They don't need to be fully developed and it's okay if they're a little crazy.</li>
-          <li>We can work with you to mold your idea into something that is currently possible with today's technology and fits your budget.</li>
+          <li>We can work with you to mold your idea into something that is both possible with today's technology and fits your budget.</li>
         </ul>
       </div>
 
@@ -22,9 +22,9 @@ const Process = (props) => {
         <h2>Research</h2>
       <div className='details'>
         <ul>
-          <li>First we do this</li>
-          <li>Then we look into that</li>
-          <li>After that theres one more thing we do</li>
+          <li>What materials can we use? How will they affect performance?</li>
+          <li>How can we design the product to be as small as possible?</li>
+          <li>What electrical components need to be integrated?</li>
         </ul>
       </div>
 
@@ -34,8 +34,8 @@ const Process = (props) => {
         <h2>Design</h2>
       <div className='details'>
         <ul>
-          <li>First we do this</li>
-          <li>Then we look into that</li>
+          <li>Generally we start with rough hand-drawn sketches in order to rapidly iterate while ensuring we're on the same page</li>
+          <li>As the design matures we move to 2D computer visuializations</li>
           <li>After that theres one more thing we do</li>
         </ul>
       </div>
