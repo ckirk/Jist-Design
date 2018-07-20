baf68b24a6ae3620c06f095863ef7e5b86014def
Chris <ckirkinis@gmail.com>
2018-07-19 20:12:34 -0400

    added a bunch of images

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -1,13 +1,23 @@
 import React, { PropTypes } from 'react';
 
 // Image Imports
-import SnagTagHero from '../images/snagtag_enclosure.png'
-import SnagTagExplode from '../images/snagtag_enclosure3.png'
+import SnagTagHero from '../images/snagtag/st_hero.png'
+import SnagTagExplode from '../images/snagtag/st_explode.png'
+import SnagTagLoop from '../images/snagtag/st_spin.gif'
+import SnagTagColors from '../images/snagtag/st_colors.jpg'
+import SnagTagLogo from '../images/snagtag/st_logo_placement.jpg'
+import SnagTagPrimitives from '../images/snagtag/st_primitives.jpg'
+import SnagTagOptions from '../images/snagtag/st_two_options.jpg'
 
-import SnagTagBoxHero from '../images/raspi_2up.png'
+import HubHero from '../images/snagtag_hub/st_hub_2x.png'
+import HubTable from '../images/snagtag_hub/st_hub_1.jpg'
+import HubTableDark from '../images/snagtag_hub/st_hub_2.jpg'
+import HubMockup from '../images/snagtag_hub/st_hub_3.jpg'
+import HubLed from '../images/snagtag_hub/st_hub_4.jpg'
+import HubLoop from '../images/snagtag_hub/st_hub_loop.gif'
 
 import RAVNgrip from '../images/GRIP-low.png'
-import RAVNcam from '../images/CAM.png'
+import RAVNcam from '../images/ravn_cam/cam.png'
 import StratHero from '../images/strat_angle.png'
 
 import SnoMote from '../images/snomote_1.png'
@@ -26,10 +36,14 @@ const Projects = (props) => {
         <p>
           Leo duis ut diam quam. Sed odio morbi quis commodo odio aenean. Sed vulputate odio ut enim blandit volutpat maecenas volutpat. Tincidunt praesent semper feugiat nibh sed pulvinar. Vulputate eu scelerisque felis imperdiet. Non arcu risus quis varius quam quisque id. Quam pellentesque nec nam aliquam sem et tortor. Hac habitasse platea dictumst quisque sagittis purus sit amet. Ut morbi tincidunt augue interdum velit euismod in. Et sollicitudin ac orci phasellus egestas tellus. Erat nam at lectus urna duis convallis convallis tellus id.
         </p>
-        <img src="" alt=""/>
+        <img src={SnagTagLoop} alt=""/>
         <p>
           Leo duis ut diam quam. Sed odio morbi quis commodo odio aenean. Sed vulputate odio ut enim blandit volutpat maecenas volutpat. Tincidunt praesent semper feugiat nibh sed pulvinar. Vulputate eu scelerisque felis imperdiet. Non arcu risus quis varius quam quisque id. Quam pellentesque nec nam aliquam sem et tortor. Hac habitasse platea dictumst quisque sagittis purus sit amet. Ut morbi tincidunt augue interdum velit euismod in. Et sollicitudin ac orci phasellus egestas tellus. Erat nam at lectus urna duis convallis convallis tellus id.
         </p>
+        <img src={SnagTagColors} alt=""/>
+        <img src={SnagTagPrimitives} alt=""/>
+        <img src={SnagTagOptions} alt=""/>
+        <img src={SnagTagLogo} alt=""/>
       </Project>
 
       <Project title='RAVN GRIP' heroImage={RAVNgrip}>
@@ -39,8 +53,19 @@ const Projects = (props) => {
         </p>
       </Project>
 
-      <Project title='iBeacon Hub' heroImage={SnagTagBoxHero}>
+      <Project title='iBeacon Hub' heroImage={HubHero}>
         <h2>Sleek wireless hub for retail data collection</h2>
+        <img src={HubMockup} alt=""/>
+        <p>
+          Leo duis ut diam quam. Sed odio morbi quis commodo odio aenean. Sed vulputate odio ut enim blandit volutpat maecenas volutpat. Tincidunt praesent semper feugiat nibh sed pulvinar. Vulputate eu scelerisque felis imperdiet. Non arcu risus quis varius quam quisque id. Quam pellentesque nec nam aliquam sem et tortor. Hac habitasse platea dictumst quisque sagittis purus sit amet. Ut morbi tincidunt augue interdum velit euismod in. Et sollicitudin ac orci phasellus egestas tellus. Erat nam at lectus urna duis convallis convallis tellus id.
+        </p>
+        <img src={HubTable} alt=""/>
+        <img src={HubLed} alt=""/>
+        <img src={HubTableDark} alt=""/>
+        <p>
+          Leo duis ut diam quam. Sed odio morbi quis commodo odio aenean. Sed vulputate odio ut enim blandit volutpat maecenas volutpat. Tincidunt praesent semper feugiat nibh sed pulvinar. Vulputate eu scelerisque felis imperdiet. Non arcu risus quis varius quam quisque id. Quam pellentesque nec nam aliquam sem et tortor. Hac habitasse platea dictumst quisque sagittis purus sit amet. Ut morbi tincidunt augue interdum velit euismod in. Et sollicitudin ac orci phasellus egestas tellus. Erat nam at lectus urna duis convallis convallis tellus id.
+        </p>
+        <img src={HubLoop} alt=""/>
       </Project>
 
       <Project title='RAVN CAM' heroImage={RAVNcam}>
@@ -48,7 +73,7 @@ const Projects = (props) => {
       </Project>
 
       <Project title='Stratocaster Guitar' heroImage={StratHero}>
-        <h2>High quality modern replica of a 1973 Fender Stratocaster</h2>
+        <h2>High quality modern replica of a 1962 Fender Stratocaster</h2>
       </Project>
 
       <Project title='SnoMote' heroImage={SnoMote}>
